25e84ab6966d32c0fb567162baafb738fc240d9d
Str4te <sephia.dk@gmail.com>
2018-04-28 17:39:28 +0200

    Add files via upload

--- g2core/settings/settings_shapeoko2.h
+++ g2core/settings/settings_shapeoko2.h
@@ -87,25 +87,25 @@
 #define MOTOR_POWER_TIMEOUT         2.00                    // motor power timeout in seconds
 
 #define M1_MOTOR_MAP                AXIS_X                  // 1ma
-#define M1_STEP_ANGLE               1.8                     // 1sa
-#define M1_TRAVEL_PER_REV           40.00                   // 1tr
-#define M1_MICROSTEPS               8                       // 1mi  1,2,4,8,16,32
+#define M1_STEP_ANGLE               0.09                     // 1sa
+#define M1_TRAVEL_PER_REV           4                   // 1tr
+#define M1_MICROSTEPS               0                       // 1mi  1,2,4,8,16,32
 #define M1_POLARITY                 0                       // 1po  0=normal, 1=reversed
 #define M1_POWER_MODE               MOTOR_POWER_MODE        // 1pm  TRUE=low power idle enabled
 #define M1_POWER_LEVEL              0.500
 
 #define M2_MOTOR_MAP                AXIS_Y
-#define M2_STEP_ANGLE               1.8
-#define M2_TRAVEL_PER_REV           40.00
-#define M2_MICROSTEPS               8
+#define M2_STEP_ANGLE               0.09
+#define M2_TRAVEL_PER_REV           4
+#define M2_MICROSTEPS               0
 #define M2_POLARITY                 0
 #define M2_POWER_MODE               MOTOR_POWER_MODE
 #define M2_POWER_LEVEL              0.500
 
 #define M3_MOTOR_MAP                AXIS_Z
-#define M3_STEP_ANGLE               1.8
-#define M3_TRAVEL_PER_REV           1.25
-#define M3_MICROSTEPS               8
+#define M3_STEP_ANGLE               0.09
+#define M3_TRAVEL_PER_REV           2
+#define M3_MICROSTEPS               0
 #define M3_POLARITY                 0
 #define M3_POWER_MODE               MOTOR_POWER_MODE
 #define M3_POWER_LEVEL              0.750
@@ -115,13 +115,13 @@
 #define JERK_MAX                    5000
 
 #define X_AXIS_MODE                 AXIS_STANDARD           // xam  see canonical_machine.h cmAxisMode for valid values
-#define X_VELOCITY_MAX              50000                   // xvm  G0 max velocity in mm/min
+#define X_VELOCITY_MAX              5100                   // xvm  G0 max velocity in mm/min
 #define X_FEEDRATE_MAX              X_VELOCITY_MAX          // xfr  G1 max feed rate in mm/min
 #define X_TRAVEL_MIN                0                       // xtn  minimum travel for soft limits
-#define X_TRAVEL_MAX                420                     // xtm  travel between switches or crashes
+#define X_TRAVEL_MAX                640                     // xtm  travel between switches or crashes
 #define X_JERK_MAX                  JERK_MAX                // xjm  jerk * 1,000,000
 #define X_JERK_HIGH_SPEED           20000                   // xjh
-#define X_HOMING_INPUT              1                       // xhi  input used for homing or 0 to disable
+#define X_HOMING_INPUT              0                       // xhi  input used for homing or 0 to disable
 #define X_HOMING_DIRECTION          0                       // xhd  0=search moves negative, 1= search moves positive
 #define X_SEARCH_VELOCITY           3000                    // xsv  minus means move to minimum switch
 #define X_LATCH_VELOCITY            100                     // xlv  mm/min
@@ -129,13 +129,13 @@
 #define X_ZERO_BACKOFF              2                       // xzb  mm
 
 #define Y_AXIS_MODE                 AXIS_STANDARD
-#define Y_VELOCITY_MAX              50000
+#define Y_VELOCITY_MAX              5100
 #define Y_FEEDRATE_MAX              Y_VELOCITY_MAX
 #define Y_TRAVEL_MIN                0
-#define Y_TRAVEL_MAX                420
+#define Y_TRAVEL_MAX                440
 #define Y_JERK_MAX                  JERK_MAX
 #define Y_JERK_HIGH_SPEED           20000
-#define Y_HOMING_INPUT              3
+#define Y_HOMING_INPUT              0
 #define Y_HOMING_DIRECTION          0
 #define Y_SEARCH_VELOCITY           3000
 #define Y_LATCH_VELOCITY            100
@@ -199,7 +199,7 @@
 #define DI4_FUNCTION                INPUT_FUNCTION_LIMIT
 
 // Zmin
-#define DI5_MODE                    IO_ACTIVE_HIGH   // Z probe
+#define DI5_MODE                    NORMALLY_CLOSED   // Z probe
 #define DI5_ACTION                  INPUT_ACTION_NONE
 #define DI5_FUNCTION                INPUT_FUNCTION_NONE
 
