e429c9a7c270e18815d156628e1f68644d144fe5
vondikall <38811315+vondikall@users.noreply.github.com>
2018-04-29 21:02:18 +0200

    File formmating
    Minor file formatting, and added a TTsettings folder for the Tool change part.

---
+++ g2core/TTsettings/tt_setup.cpp
@@ -0,0 +1,6 @@
+/*
+ * tt_setup.cpp
+ *
+ * Created: 29.4.2018 21:00:00
+ *  Author: Vondikall
+ */ 
---
+++ g2core/TTsettings/tt_setup.h
@@ -0,0 +1,16 @@
+/*
+ * tt_setup.h
+ *
+ * Created: 29.4.2018 21:00:23
+ *  Author: Vondikall
+ */ 
+
+
+#ifndef TT_SETUP_H_
+#define TT_SETUP_H_
+
+
+
+
+
+#endif /* TT_SETUP_H_ */
